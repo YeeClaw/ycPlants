3940f7afec756124d318b9dbe3b9299eda20df73
Austin Colt <austin@jcolt.net>
2020-07-30 12:34:52 -0600

    Fixes cut off sprites for StarPlant

--- Tiles/Plants/StarPlant.cs
+++ Tiles/Plants/StarPlant.cs
@@ -6,7 +6,7 @@ using Terraria.ObjectData;
 
 namespace ycPlants.Tiles.Plants
 {
-    public class s1StarPlant : ModTile
+    public class StarPlant : ModTile
     {
         public override void SetDefaults()
         {

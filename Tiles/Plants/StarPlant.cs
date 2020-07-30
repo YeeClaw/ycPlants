0e375e7f3fa1e35ef33c2ff45451e8e53acba8da
Austin Colt <austin@jcolt.net>
2020-07-30 12:57:21 -0600

    Adds growth enum and sprite flip

--- Tiles/Plants/StarPlant.cs
+++ Tiles/Plants/StarPlant.cs
@@ -3,26 +3,48 @@ using Terraria;
 using Microsoft.Xna.Framework;
 using Terraria.ModLoader;
 using Terraria.ObjectData;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace ycPlants.Tiles.Plants
 {
+    public enum PlantStage : byte
+    {
+        Planted,
+        Growing,
+        Grown
+    }
+
     public class StarPlant : ModTile
     {
         public override void SetDefaults()
         {
+            // plant properties
             Main.tileSolid[Type] = false;
             Main.tileLavaDeath[Type] = true;
             Main.tileWaterDeath[Type] = true;
             Main.tileBlockLight[Type] = false;
             Main.tileFrameImportant[Type] = true;
+            Main.tileCut[Type] = true;
+            Main.tileNoFail[Type] = true;
 
+            //
             TileObjectData.newTile.CopyFrom(TileObjectData.Style1x2);
             TileObjectData.addTile(Type);
 
+            // map visibility
             ModTranslation name = CreateMapEntryName();
             name.SetDefault("Star crop");
 
             AddMapEntry(new Color(236, 247, 20), name);
         }
+
+        // changes direction depending on where the player is facing (currently not functional
+        public override void SetSpriteEffects(int i, int j, ref SpriteEffects spriteEffects)
+        {
+            if (i % 2 == 1)
+                spriteEffects = SpriteEffects.FlipHorizontally;
+        }
+
+
     }
 }

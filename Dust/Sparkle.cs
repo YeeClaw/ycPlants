f17528c22aa86dc4bdd06e9dd672547802b34a76
Austin Colt <austin@jcolt.net>
2020-08-01 17:30:44 -0600

    Adds sparkle dust

--- Dust/Sparkle.cs
+++ Dust/Sparkle.cs
@@ -6,6 +6,27 @@ namespace ycPlants.Dust
 {
     public class Sparkle : ModDust
     {
+        public override void OnSpawn(Terraria.Dust dust)
+        {
+            dust.velocity *= 0.4f;
+            dust.noGravity = true;
+            dust.noLight = true;
+            dust.scale *= 1.5f;
+        }
 
+        public override bool Update(Terraria.Dust dust)
+        {
+            dust.position += dust.velocity;
+            dust.rotation += dust.velocity.X * 015f;
+            dust.scale *= 0.99f;
+            float light = 0.35f * dust.scale;
+            Lighting.AddLight(dust.position, light, light, light);
+            if (dust.scale < 0.5f)
+            {
+                dust.active = false;
+            }
+            return false;
+
+        }
     }
 }

884d74eb174cf7f088d587e1f00bd2962dd5b5dc
Austin Colt <austin@jcolt.net>
2020-08-07 14:53:43 -0600

    Adds unfinished SetDefaults for LeadPlant

--- Tiles/Plants/LeadPlant.cs
+++ Tiles/Plants/LeadPlant.cs
@@ -14,8 +14,48 @@ namespace ycPlants.Tiles.Plants
 {
     public class LeadPlant : ModTile
     {
+        private const int frameWidth = 18;
 
+        public override void SetDefaults()
+        {
+            // plant properties
+            Main.tileSolid[Type] = false;
+            Main.tileLavaDeath[Type] = false;
+            Main.tileWaterDeath[Type] = false;
+            Main.tileBlockLight[Type] = false;
+            Main.tileFrameImportant[Type] = true;
+            Main.tileNoFail[Type] = true;
+            Main.tileNoAttach[Type] = true;
+            Main.tileLighted[Type] = true;
+            Main.tileCut[Type] = false;
 
+            // adds tile
+            TileObjectData.newTile.Width = 1;
+            TileObjectData.newTile.Height = 2;
+            TileObjectData.newTile.Origin = new Point16(0, 1);
+            TileObjectData.newTile.UsesCustomCanPlace = true;
+            TileObjectData.newTile.AnchorTop = new AnchorData(AnchorType.SolidTile | AnchorType.SolidSide, TileObjectData.newTile.Width, 0);
+            TileObjectData.newTile.CoordinateHeights = new int[2]
+            {
+                16,
+                16
+            };
+            TileObjectData.newTile.CoordinateWidth = 16;
+            TileObjectData.newTile.CoordinatePadding = 2;
+            TileObjectData.newTile.LavaDeath = false;
+            TileObjectData.newTile.DrawYOffset = 2;
+            TileObjectData.newTile.StyleHorizontal = true;
+            dustType = DustType<Sparkle>();
+            TileObjectData.newTile.AnchorValidTiles = new int[]
+            {
+                TileID.Lead
+            };
+            TileObjectData.addTile(Type);
+        }
 
+        public override void SetSpriteEffects(int i, int j, ref SpriteEffects spriteEffects)
+        {
+            spriteEffects = SpriteEffects.FlipVertically;
+        }
     }
 }

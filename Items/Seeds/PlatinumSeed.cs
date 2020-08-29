5e00cc2cfe50ffce11f7fe1f5c7cd734cd3dd94b
Austin Colt <austin@jcolt.net>
2020-08-29 13:47:32 -0600

    Fully implements the platinum stalactite and seed

---
+++ Items/Seeds/PlatinumSeed.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using static Terraria.ModLoader.ModContent;
+
+namespace ycPlants.Items.Seeds
+{
+    class PlatinumSeed : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Tooltip.SetDefault("Not worth its weight in platinum! \n(Grows upside down on platinum ore)");
+        }
+
+        public override void SetDefaults()
+        {
+            item.height = 18;
+            item.width = 20;
+            item.autoReuse = true;
+            item.useTurn = true;
+            item.useStyle = ItemUseStyleID.SwingThrow;
+            item.useAnimation = 15;
+            item.useTime = 10;
+            item.maxStack = 99;
+            item.consumable = true;
+            item.placeStyle = 0;
+            item.createTile = TileType<Tiles.Plants.PlatinumPlant>();
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.PlatinumOre, 3);
+            recipe.AddIngredient(ItemID.StoneBlock, 2);
+            recipe.AddTile(TileID.Bottles);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}

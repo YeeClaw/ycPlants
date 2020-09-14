36d1fa0fbfa32342c5f56331d69cc56303df2a03
Austin Colt <austin@jcolt.net>
2020-09-14 16:10:57 -0600

    Fully adds silver seed and respective [crop]

---
+++ Items/Seeds/SilverSeed.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using static Terraria.ModLoader.ModContent;
+
+namespace ycPlants.Items.Seeds
+{
+    class SilverSeed : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Tooltip.SetDefault("Actually worth silver! \n(Grows upside down on silver ore)");
+        }
+
+        public override void SetDefaults()
+        {
+            item.height = 18;
+            item.width = 20;
+            item.autoReuse = true;
+            item.useTurn = true;
+            item.useStyle = ItemUseStyleID.SwingThrow;
+            item.useAnimation = 15;
+            item.useTime = 10;
+            item.maxStack = 99;
+            item.consumable = true;
+            item.placeStyle = 0;
+            item.createTile = TileType<Tiles.Plants.SilverPlant>();
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.SilverOre, 3);
+            recipe.AddIngredient(ItemID.StoneBlock, 2);
+            recipe.AddTile(TileID.Bottles);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
+

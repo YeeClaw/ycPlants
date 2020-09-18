670f5a46b13c89c30641c94bd4a53d0fd4002300
Austin Colt <austin@jcolt.net>
2020-09-18 15:25:17 -0600

    Finalize cobalt seed

--- Items/Seeds/Stalseeds/CobaltSeed.cs
+++ Items/Seeds/Stalseeds/CobaltSeed.cs
@@ -1,12 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using static Terraria.ModLoader.ModContent;
 
 namespace ycPlants.Items.Seeds
 {
-    class CobaltSeed
+    class CobaltSeed : ModItem
     {
+        public override void SetStaticDefaults()
+        {
+            Tooltip.SetDefault("Keep away from magnets! \n(Grows upside down on cobalt ore)");
+        }
+
+        public override void SetDefaults()
+        {
+            item.height = 18;
+            item.width = 20;
+            item.autoReuse = true;
+            item.useTurn = true;
+            item.useStyle = ItemUseStyleID.SwingThrow;
+            item.useAnimation = 15;
+            item.useTime = 10;
+            item.maxStack = 99;
+            item.consumable = true;
+            item.placeStyle = 0;
+            item.createTile = TileType<Tiles.Plants.CobaltPlant>();
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.CobaltOre, 5);
+            recipe.AddIngredient(ItemID.StoneBlock, 2);
+            recipe.AddTile(TileID.Bottles);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
     }
 }

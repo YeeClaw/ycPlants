e75c7614a9944901418bd096f1848594cf1062dd
Austin Colt <austin@jcolt.net>
2020-09-18 15:43:10 -0600

    Finalize orichalcum seed

--- Items/Seeds/Stalseeds/OriSeed.cs
+++ Items/Seeds/Stalseeds/OriSeed.cs
@@ -1,12 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using static Terraria.ModLoader.ModContent;
 
 namespace ycPlants.Items.Seeds
 {
-    class OriSeed
+    class OriSeed : ModItem
     {
+        public override void SetStaticDefaults()
+        {
+            Tooltip.SetDefault("It's not actually pink! \n(Grows upside down on orichalcum ore)");
+        }
+
+        public override void SetDefaults()
+        {
+            item.height = 18;
+            item.width = 20;
+            item.autoReuse = true;
+            item.useTurn = true;
+            item.useStyle = ItemUseStyleID.SwingThrow;
+            item.useAnimation = 15;
+            item.useTime = 10;
+            item.maxStack = 99;
+            item.consumable = true;
+            item.placeStyle = 0;
+            item.createTile = TileType<Tiles.Plants.OriPlant>();
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.OrichalcumOre, 5);
+            recipe.AddIngredient(ItemID.StoneBlock, 2);
+            recipe.AddTile(TileID.Bottles);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
     }
 }
